ecae82b357ced8d50e88959c86ddd84ab227fae7
cgz <chenguanzhou123@163.com>
2021-06-23 17:11:31 +0800

    new metric

--- evaluate_gt.py
+++ evaluate_gt.py
@@ -1,7 +1,7 @@
 import numpy as np
 from osgeo import gdal_array
-from sklearn import metrics
-
+from sklearn.metrics import adjusted_mutual_info_score, fowlkes_mallows_score
+from skimage.metrics import adapted_rand_error
 def run():
     # gt_path = "data/austin26/gt.tif"
     # pred_path = "results/{}/{}/{}".format("austin26", "512", "test_pixel_epoch_7.tif")
@@ -12,19 +12,27 @@ def run():
     # pred_path = "results/top_potsdam_3_10/isodata.img"
 
     gt_path = "data/evlab44/gt.tif"
-    # pred_path = "results/{}/{}/{}".format("evlab44", "21-06-22_23-09-29", "test_pixel_epoch_2.tif")
-    pred_path = "results/evlab44/isodata.img"
+    pred_path = "results/{}/{}/{}".format("evlab44", "21-06-22_23-09-29", "test_pixel_epoch_2.tif")
+    # pred_path = "results/evlab44/kmeans.img"
 
 
-    gt = gdal_array.LoadFile(gt_path).flatten()
-    pred = gdal_array.LoadFile(pred_path).flatten()
+    gt = gdal_array.LoadFile(gt_path)
+    pred = gdal_array.LoadFile(pred_path)
 
-    assert len(gt) == len(pred)
+    # assert len(gt) == len(pred)
+    assert gt.shape == pred.shape
 
+    ARE,_,_ = adapted_rand_error(gt, pred)
+    print("ARE", ARE)
+
+
+    gt = gt.flatten()
+    pred = pred.flatten()
 
     # ARI = metrics.adjusted_rand_score(gt, pred)  
-    AMI = metrics.adjusted_mutual_info_score(gt, pred)  
-    FMI = metrics.fowlkes_mallows_score(gt, pred)  
+    AMI = adjusted_mutual_info_score(gt, pred)  
+    FMI = fowlkes_mallows_score(gt, pred)  
+
 
     # print("ARI", ARI)
     print("AMI", AMI)
